76c03d94268a49d1494f76f08097e255f061bab9
sejo <josemagnoajero@gmail.com>
2025-03-26 03:48:19 +0000

    -added declare module in src/vite-env.d.ts -deleted redundant Login.vue (LoginView.vue instead)

--- src/router.ts
+++ src/router.ts
@@ -13,35 +13,8 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
-  routes: [
-    {
-      path: '/',
-      redirect: '/login'
-    },
-    {
-      path: '/login',
-      name: 'Login',
-      component: Login
-    },
-    {
-      path: '/home',
-      name: 'Home',
-      component: HomeView,
-      meta: { requiresAuth: true }
-    }
-  ]
-});
-
-router.beforeEach((to, from, next) => {
-  // Initialize PocketBase inside the guard
-  const pb = new PocketBase('http://127.0.0.1:8090');
-  
-  if (to.meta.requiresAuth && !pb.authStore.isValid) {
-    next('/login');
-  } else {
-    next();
-  }
+    history: createWebHistory(),
+    routes,
 });
 
 export default router;
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,9 @@
 /// <reference types="vite/client" />
+// src/vite-env.d.ts
+/// <reference types="vite/client" />
+
+declare module '*.vue' {
+    import type { DefineComponent } from 'vue'
+    const component: DefineComponent<{}, {}, any>
+    export default component
+  }
